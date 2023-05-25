be802d6ccc5c04d6601e650d744e327b11ec8ce7
geekrope <geekrope@gmail.com>
2023-05-25 23:39:12 +0400

    Implemented image scaling

--- mandelbrotSet/app.js
+++ mandelbrotSet/app.js
@@ -17,7 +17,7 @@ var ComplexNumber = /** @class */ (function () {
         enumerable: false,
         configurable: true
     });
-    Object.defineProperty(ComplexNumber.prototype, "absolute", {
+    Object.defineProperty(ComplexNumber.prototype, "absoluteValue", {
         get: function () {
             return Math.sqrt(this._real * this._real + this._imaginary * this._imaginary);
         },
@@ -31,40 +31,72 @@ var ComplexNumber = /** @class */ (function () {
         return new ComplexNumber(z1.real * z2.real - z1.imaginary * z2.imaginary, z1.real * z2.imaginary + z1.imaginary * z2.real);
     };
     ComplexNumber.square = function (z) {
-        return ComplexNumber.multiply(z, z);
+        return new ComplexNumber(z.real * z.real - z.imaginary * z.imaginary, 2 * z.real * z.imaginary);
     };
     return ComplexNumber;
 }());
-var scope = { x: -2, y: -2, width: 4, height: 4 };
-var z_0 = new ComplexNumber(0, 0);
-function isInSet(itters, point) {
+var ComputingEngine = /** @class */ (function () {
+    function ComputingEngine() {
+    }
+    ComputingEngine.computeFractalPoints = function (belongsToFractal, step, itterations, scope) {
+        var points = [];
+        for (var x = scope.x; x < scope.x2; x += step) {
+            for (var y = scope.y; y < scope.y2; y += step) {
+                var point = new ComplexNumber(x, y);
+                if (belongsToFractal(itterations, point)) {
+                    points.push(point);
+                }
+            }
+        }
+        return points;
+    };
+    return ComputingEngine;
+}());
+var Transform = /** @class */ (function () {
+    function Transform() {
+    }
+    Transform.computeFromMatrix = function (transformedPoint, matrix) {
+        var x = (-matrix.c * matrix.f + matrix.c * transformedPoint.y + matrix.d * matrix.e - matrix.d * transformedPoint.x) / (matrix.b * matrix.c - matrix.a * matrix.d);
+        var y = (-matrix.b * matrix.e + matrix.b * transformedPoint.x + matrix.a * matrix.f - matrix.a * transformedPoint.y) / (matrix.b * matrix.c - matrix.a * matrix.d);
+        return new DOMPoint(x, y);
+    };
+    Transform.computeMatrix = function (relativePoint, scale) {
+        return new DOMMatrix([scale, 0, 0, scale, relativePoint.x * (1 - scale), relativePoint.y * (1 - scale)]);
+    };
+    return Transform;
+}());
+var mandelbrotSet = function (itterations, point) {
     var z = new ComplexNumber(0, 0);
     var belongs = true;
-    for (var itter = 0; itter < itters && belongs; itter++) {
+    for (var itteration = 0; itteration < itterations && belongs; itteration++) {
         z = ComplexNumber.sum(ComplexNumber.square(z), point);
-        if (z.absolute > 2) {
+        if (z.absoluteValue > 2) {
             belongs = false;
         }
     }
     return belongs;
-}
-function draw(ctx) {
-    var d = 0.001;
-    var itters = 100;
-    var points = [];
-    for (var x = scope.x; x < scope.width + scope.x; x += d) {
-        for (var y = scope.y; y < scope.height + scope.y; y += d) {
-            var point = new ComplexNumber(x, y);
-            if (isInSet(itters, point)) {
-                points.push(point);
-            }
-        }
-    }
-    points.forEach(function (point) { ctx.fillRect(point.real * 200 + 500, point.imaginary * 200 + 500, 1, 1); });
+};
+function draw(ctx, transform, scale) {
+    var frameEdge1 = Transform.computeFromMatrix(new DOMPoint(0, 0), transform);
+    var frameEdge2 = Transform.computeFromMatrix(new DOMPoint(1920, 1080), transform);
+    ctx.clearRect(frameEdge1.x, frameEdge1.y, frameEdge2.x - frameEdge1.x, frameEdge2.y - frameEdge1.y);
+    var points = ComputingEngine.computeFractalPoints(mandelbrotSet, 1 / scale, 10, { x: -2, y: -2, x2: 2, y2: 2 });
+    points.forEach(function (point) { ctx.fillRect(point.real, point.imaginary, 1 / scale, 1 / scale); });
 }
 window.onload = function () {
     var cnvs = document.getElementById("cnvs");
     var ctx = cnvs.getContext("2d");
-    draw(ctx);
+    var scale = 100;
+    var transform = new DOMMatrix([scale, 0, 0, scale, 500, 500]);
+    ctx.setTransform(transform);
+    draw(ctx, transform, scale);
+    cnvs.onmousedown = function (e) {
+        scale += 100;
+        transform = Transform.computeMatrix(Transform.computeFromMatrix(new DOMPoint(e.x, e.y), transform), scale);
+        transform.e += e.x;
+        transform.f += e.y;
+        ctx.setTransform(transform);
+        draw(ctx, transform, scale);
+    };
 };
 //# sourceMappingURL=app.js.map
--- mandelbrotSet/app.ts
+++ mandelbrotSet/app.ts
@@ -1,32 +1,35 @@
-﻿class ComplexNumber
+﻿type rectangle = { x: number, y: number, x2: number, y2: number };
+type belongsToFractal = (itterations: number, point: ComplexNumber) => boolean;
+
+class ComplexNumber
 {
 	private _real: number;
 	private _imaginary: number;
 
-	public get real()
+	public get real(): number
 	{
 		return this._real;
 	}
-	public get imaginary()
+	public get imaginary(): number
 	{
 		return this._imaginary;
 	}
-	public get absolute()
+	public get absoluteValue(): number
 	{
 		return Math.sqrt(this._real * this._real + this._imaginary * this._imaginary)
 	}
 
-	public static sum(z1: ComplexNumber, z2: ComplexNumber)
+	public static sum(z1: ComplexNumber, z2: ComplexNumber): ComplexNumber
 	{
 		return new ComplexNumber(z1.real + z2.real, z1.imaginary + z2.imaginary);
 	}
-	public static multiply(z1: ComplexNumber, z2: ComplexNumber)
+	public static multiply(z1: ComplexNumber, z2: ComplexNumber): ComplexNumber
 	{
 		return new ComplexNumber(z1.real * z2.real - z1.imaginary * z2.imaginary, z1.real * z2.imaginary + z1.imaginary * z2.real);
 	}
-	public static square(z: ComplexNumber)
+	public static square(z: ComplexNumber): ComplexNumber
 	{
-		return ComplexNumber.multiply(z, z);
+		return new ComplexNumber(z.real * z.real - z.imaginary * z.imaginary, 2 * z.real * z.imaginary);
 	}
 
 	public constructor(real: number, imaginary: number)
@@ -36,19 +39,54 @@
 	}
 }
 
-const scope = { x: -2, y: -2, width: 4, height: 4 };
-const z_0 = new ComplexNumber(0, 0);
+class ComputingEngine
+{
+	public static computeFractalPoints(belongsToFractal: belongsToFractal, step: number, itterations: number, scope: rectangle)
+	{
+		const points: ComplexNumber[] = [];
 
-function isInSet(itters: number, point: ComplexNumber)
+		for (let x = scope.x; x < scope.x2; x += step)
+		{
+			for (let y = scope.y; y < scope.y2; y += step)
+			{
+				const point = new ComplexNumber(x, y);
+
+				if (belongsToFractal(itterations, point))
+				{
+					points.push(point);
+				}
+			}
+		}
+
+		return points;
+	}
+}
+
+class Transform
+{
+	public static computeFromMatrix(transformedPoint: DOMPoint, matrix: DOMMatrix): DOMPoint
+	{
+		const x = (-matrix.c * matrix.f + matrix.c * transformedPoint.y + matrix.d * matrix.e - matrix.d * transformedPoint.x) / (matrix.b * matrix.c - matrix.a * matrix.d);
+		const y = (-matrix.b * matrix.e + matrix.b * transformedPoint.x + matrix.a * matrix.f - matrix.a * transformedPoint.y) / (matrix.b * matrix.c - matrix.a * matrix.d)
+
+		return new DOMPoint(x, y);
+	}
+	public static computeMatrix(relativePoint: DOMPoint, scale: number)
+	{
+		return new DOMMatrix([scale, 0, 0, scale, relativePoint.x * (1 - scale), relativePoint.y * (1 - scale)]);
+	}
+}
+
+const mandelbrotSet: belongsToFractal = (itterations: number, point: ComplexNumber) =>
 {
 	let z = new ComplexNumber(0, 0);
 	let belongs = true;
 
-	for (let itter = 0; itter < itters && belongs; itter++)
+	for (let itteration = 0; itteration < itterations && belongs; itteration++)
 	{
 		z = ComplexNumber.sum(ComplexNumber.square(z), point);
 
-		if (z.absolute > 2)
+		if (z.absoluteValue > 2)
 		{
 			belongs = false;
 		}
@@ -57,25 +95,16 @@ function isInSet(itters: number, point: ComplexNumber)
 	return belongs;
 }
 
-function draw(ctx: CanvasRenderingContext2D)
+function draw(ctx: CanvasRenderingContext2D, transform: DOMMatrix, scale)
 {
-	const d = 0.001;
-	const itters = 100;
-	const points: ComplexNumber[] = [];
-	for (let x = scope.x; x < scope.width + scope.x; x += d)
-	{
-		for (let y = scope.y; y < scope.height + scope.y; y += d)
-		{
-			const point = new ComplexNumber(x, y);
+	const frameEdge1 = Transform.computeFromMatrix(new DOMPoint(0, 0), transform);
+	const frameEdge2 = Transform.computeFromMatrix(new DOMPoint(1920, 1080), transform);
 
-			if (isInSet(itters, point))
-			{
-				points.push(point);
-			}
-		}
-	}
+	ctx.clearRect(frameEdge1.x, frameEdge1.y, frameEdge2.x - frameEdge1.x, frameEdge2.y - frameEdge1.y);
 
-	points.forEach((point) => { ctx.fillRect(point.real * 200 + 500, point.imaginary * 200 + 500, 1, 1) });
+	const points = ComputingEngine.computeFractalPoints(mandelbrotSet, 1 / scale, 10, { x: - 2, y: -2, x2: 2, y2: 2 });
+
+	points.forEach((point) => { ctx.fillRect(point.real, point.imaginary, 1 / scale, 1 / scale) });
 }
 
 window.onload = () =>
@@ -83,6 +112,23 @@ window.onload = () =>
 	const cnvs: HTMLCanvasElement = document.getElementById("cnvs") as HTMLCanvasElement;
 	const ctx = cnvs.getContext("2d");
 
-	draw(ctx);
+	let scale = 100;
+	let transform = new DOMMatrix([scale, 0, 0, scale, 500, 500]);
 
+	ctx.setTransform(transform);
+
+	draw(ctx, transform, scale);
+
+	cnvs.onmousedown = (e) =>
+	{
+		scale += 100;
+
+		transform = Transform.computeMatrix(Transform.computeFromMatrix(new DOMPoint(e.x, e.y), transform), scale);
+
+		transform.e += e.x;
+		transform.f += e.y;
+
+		ctx.setTransform(transform);
+		draw(ctx, transform, scale);
+	}
 }
